f42a4526ebf0b6ce93985f2d1831c6236baa8d5a
mikhail0777 <77640183+mikhail0777@users.noreply.github.com>
2026-08-30 14:15:20 -0400

    feat(core): implement log parser core and query parameter decoding

---
+++ lib/LogParser/Core.pm
@@ -0,0 +1,98 @@
+﻿package LogParser::Core;
+
+use strict;
+use warnings;
+use Carp qw(croak);
+
+our $VERSION = '1.0.0';
+
+# Standard Nginx/Apache Combined Log Format (CLF)
+my $LOG_REGEX = qr{
+    ^
+    (?P<ip>\S+)\s+
+    (?P<ident>\S+)\s+
+    (?P<authuser>\S+)\s+
+    \[(?P<timestamp>[^\]]+)\]\s+
+    "(?P<method>[A-Z]+)\s+
+     (?P<request>[^\s"]+)\s*
+     (?P<protocol>[^"]*)"\s+
+    (?P<status>\d{3})\s+
+    (?P<bytes>\S+)
+    (?:
+        \s+"(?P<referer>[^"]*)"\s+
+        "(?P<user_agent>[^"]*)"
+    )?
+    $
+}x;
+
+sub new {
+    my ($class, %args) = @_;
+    my $self = {
+        filter_pattern => defined $args{filter} ? qr/$args{filter}/i : undef,
+        target_params  => $args{params} || [],
+    };
+    return bless $self, $class;
+}
+
+sub parse_line {
+    my ($self, $line) = @_;
+    return undef unless defined $line;
+
+    if (defined $self->{filter_pattern}) {
+        return undef unless $line =~ $self->{filter_pattern};
+    }
+
+    if ($line =~ $LOG_REGEX) {
+        my %entry = (
+            ip         => $+{ip},
+            authuser   => $+{authuser},
+            timestamp  => $+{timestamp},
+            method     => $+{method},
+            request    => $+{request},
+            protocol   => $+{protocol} || '',
+            status     => int($+{status}),
+            bytes      => ($+{bytes} eq '-' ? 0 : int($+{bytes})),
+            referer    => $+{referer} || '',
+            user_agent => $+{user_agent} || '',
+        );
+
+        $entry{params} = $self->_extract_query_params($entry{request});
+        return \%entry;
+    }
+
+    return undef;
+}
+
+sub _extract_query_params {
+    my ($self, $uri) = @_;
+    my %params;
+
+    if ($uri =~ /\?(.+)$/) {
+        my $query_string = $1;
+        for my $pair (split /&/, $query_string) {
+            my ($key, $val) = split /=/, $pair, 2;
+            next unless defined $key;
+            $val = '' unless defined $val;
+            
+            # RFC 3986 decode
+            $key =~ tr/+/ /;
+            $key =~ s/%([a-fA-F0-9]{2})/chr(hex($1))/eg;
+            $val =~ tr/+/ /;
+            $val =~ s/%([a-fA-F0-9]{2})/chr(hex($1))/eg;
+
+            $params{$key} = $val;
+        }
+    }
+
+    if (@{ $self->{target_params} }) {
+        my %filtered;
+        for my $p (@{ $self->{target_params} }) {
+            $filtered{$p} = $params{$p} // '';
+        }
+        return \%filtered;
+    }
+
+    return \%params;
+}
+
+1;
---
+++ t/01_core.t
@@ -0,0 +1,41 @@
+﻿use strict;
+use warnings;
+use Test::More tests => 5;
+
+use_ok('LogParser::Core');
+
+my $sample = '192.168.1.100 - user [10/Oct/2026:13:55:36 -0700] ' .
+             '"GET /searchbox?engine=solr&query=amd+epyc&subengine=hardware HTTP/1.1" ' .
+             '200 4512 "https://example.com" "Mozilla/5.0"';
+
+subtest 'Combined log parsing' => sub {
+    my $parser = LogParser::Core->new();
+    my $res = $parser->parse_line($sample);
+
+    is($res->{ip}, '192.168.1.100', 'IP extracted');
+    is($res->{status}, 200, 'Status code parsed as integer');
+    is($res->{bytes}, 4512, 'Bytes parsed');
+    is($res->{params}{engine}, 'solr', 'Query param matched');
+    is($res->{params}{query}, 'amd epyc', 'URL decoding verified');
+};
+
+subtest 'Keyword filter' => sub {
+    my $parser = LogParser::Core->new(filter => 'searchbox');
+    ok($parser->parse_line($sample), 'Matches filter pattern');
+
+    my $parser_mismatch = LogParser::Core->new(filter => 'admin');
+    is($parser_mismatch->parse_line($sample), undef, 'Rejects non-matching line');
+};
+
+subtest 'Parameter filtering' => sub {
+    my $parser = LogParser::Core->new(params => ['engine', 'subengine']);
+    my $res = $parser->parse_line($sample);
+
+    is_deeply([sort keys %{$res->{params}}], ['engine', 'subengine'], 'Filtered exact keys');
+    is($res->{params}{subengine}, 'hardware', 'Param value matched');
+};
+
+subtest 'Invalid line handling' => sub {
+    my $parser = LogParser::Core->new();
+    is($parser->parse_line('malformed log data'), undef, 'Returns undef on unparseable lines');
+};
